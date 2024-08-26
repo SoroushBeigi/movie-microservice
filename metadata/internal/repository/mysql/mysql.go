24f4c9e46b12591854ec74a6c8a0ad7f8f268de2
s.masoumbeigi <sbeygi712@gmail.com>
2024-08-26 16:33:47 +0330

    implemented get and put for mysql repository in metadata service

--- metadata/internal/repository/mysql/mysql.go
+++ metadata/internal/repository/mysql/mysql.go
@@ -1,6 +1,11 @@
 package mysql
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"github.com/SoroushBeigi/movie-microservice/metadata/internal/repository"
+	"github.com/SoroushBeigi/movie-microservice/metadata/pkg/model"
+)
 
 type Repository struct {
 	db *sql.DB
@@ -13,5 +18,28 @@ func New() (repository *Repository, err error) {
 		return nil, err
 	}
 	return &Repository{db}, nil
+}
 
+func (r *Repository) Get(ctx context.Context, id string) (*model.Metadata, error) {
+	var title, description, director string
+	row := r.db.QueryRowContext(ctx, "SELECT title,description,director FROM movies WHERE id = ?", id)
+	if err := row.Scan(&title, &description, &director); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, repository.ErrNotFound
+		}
+		return nil, err
+	}
+	return &model.Metadata{
+		ID:          id,
+		Title:       title,
+		Description: description,
+		Director:    director,
+	}, nil
+}
+
+func (r *Repository) Put(ctx context.Context, id string, metadata *model.Metadata) error {
+	_, err := r.db.ExecContext(ctx,
+		"INSERT INTO movies (id,title,description,director) VALUES (?,?,?,?)",
+		id, metadata.Title, metadata.Description, metadata.Director)
+	return err
 }
